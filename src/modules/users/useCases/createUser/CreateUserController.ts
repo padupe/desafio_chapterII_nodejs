b0a22134e2e8ba44d34af55d53b357d36c506a31
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-10 14:28:45 -0300

    refactor(Controller): change in CreateUserController.ts file code.

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -7,10 +7,13 @@ class CreateUserController {
 
   handle(request: Request, response: Response): Response {
     const { name, email } = request.body;
+    try {
+      const newUser = this.createUserUseCase.execute({ name, email });
 
-    this.createUserUseCase.execute({ name, email });
-
-    return response.status(201).send();
+      return response.status(201).json(newUser);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 

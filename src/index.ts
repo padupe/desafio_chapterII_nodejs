448ef5eb898759ff6f57a3dd74693c42b4b120e2
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-10 18:11:35 -0300

    feat(Swagger): Dynamically generated API documentation.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/desafio_api-docs", swagger.serve, swagger.setup(swaggerConfig));
 
 app.use("/users", usersRoutes);
 

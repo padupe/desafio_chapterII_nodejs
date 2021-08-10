06567dbdbd158862153cbd9bc77fc03bd86f3715
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-10 15:41:25 -0300

    feat(useCases/listAllUsers): Controller and UseCase  added to Project.

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,8 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    const showUser = this.usersRepository.list(({ user_id } = []));
-    return showUser;
+    // Preciso verificar se o id existe, por isso usamos o método 'findById'
+    const isAdmin = this.usersRepository.findById(user_id);
+
+    // Se o id for INVÁLIDO, 'cai' no if
+    if (!isAdmin) {
+      throw new Error("User not found!");
+      // Se o usuário não tiver permissão de Administrador 'cai' no if
+    } else if (!isAdmin.admin) {
+      throw new Error("You do not have Administrator privileges.");
+    }
+
+    // Após as duas validações, o usuário irá conseguir verificar a lista de usuários
+    return this.usersRepository.list();
   }
 }
 

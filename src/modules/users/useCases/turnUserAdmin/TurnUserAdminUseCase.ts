553acdfda72424f2a3fab4e00f44d0bf871efcdd
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-10 15:00:27 -0300

    fix(TurnUserAdmin): correction of typing and syntax.
    There was a need to call the findByID method before making the change.

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -2,14 +2,14 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: User;
+  user_id: string;
 }
 
 class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    const userADM = this.usersRepository.turnAdmin(user_id);
+    const userADM = this.usersRepository.findById(user_id);
 
     if (!userADM) {
       throw new Error("User not found!");

eb58aded7f77b408b192a796de77682181d9a9e4
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-10 15:06:38 -0300

    feat(useCases/showUserProfile): Controller added to Project.

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,14 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const getUser = this.showUserProfileUseCase.execute({ user_id });
+      return response.status(200).json({ getUser });
+    } catch (error) {
+      return response.status(400).json({ error: "User not found!" });
+    }
   }
 }
 
